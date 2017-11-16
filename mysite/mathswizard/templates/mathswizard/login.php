ccfc92e3d0abb5fad9723e49295b503775060153
JayH117 <C14403052@mydit.ie>
2017-11-16 21:21:10 +0000

    Templates for login added, urls and views updated

--- mysite/mathswizard/templates/mathswizard/login.php
+++ mysite/mathswizard/templates/mathswizard/login.php
@@ -2,40 +2,7 @@
 
 {% block content %}
 <body>
-<?php
-	
-	$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server
-	$db = mysql_select_db("assignment", $connection); // Selecting Database from Server
-	
-	
-	//coming from signup page message
-	if (isset($_GET['Message'])) 
-	{
-		print '<script type="text/javascript">alert("' . $_GET['Message'] . '");</script>';
-	}
-	//logging in the user by checking information in the database
-	if(isset($_POST['submit']))
-	{ 
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		
-		$check = mysql_query("SELECT * FROM users WHERE Username = '$_POST[username]' AND Password  = '$_POST[password]'");
-	
-		if(mysql_num_rows($check)> 0) 
-		{
-			session_start();
-			$_SESSION["activeuser"] = "$username";
-			header("Location: myhome.php");
-		}
-		
-		else
-		{
-			echo '<script language="javascript">';
-			echo 'alert("Error: Username or password is incorrect")';
-			echo '</script>';
-		}
-	}	
-?>
+
 <div id = "header">
 	<img src="The Library Logo.jpg" style="width:270px;height:125px;">
 	<img src="banner.jpg" style="width:700px;height:150px;paddingtop:30px" align="right">
@@ -49,6 +16,7 @@
 		<div id = "incon">
 			<h2>Please enter your Username and Password to log in</h2>
 			<form action="" method="post">
+				{% csrf_token %}
 				<label>UserName :</label>
 				<input id="username" name="username" placeholder="Username" type="text">
 				<label>Password :</label>
--- mysite/mathswizard/views.py
+++ mysite/mathswizard/views.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 
-def index(request):
+def home(request):
 	return render(request, 'mathswizard/home.html')
 
+def login(request):
+	return render(request, 'mathswizard/login.php')
+	
+# def logout(request):
+	# return render(request, 'mathswizard/logout.php')
 # Create your views here.
